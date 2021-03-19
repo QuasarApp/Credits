15190246fbe25e4b9a4864214b756b546145f40f
EndrII <EndrIIMail@gmail.com>
2021-03-19 21:37:38 +0300

    fix translations

--- src/Library/languages/en.ts
+++ src/Library/languages/en.ts
@@ -4,37 +4,37 @@
 <context>
     <name>Credits</name>
     <message>
-        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="14"/>
+        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="31"/>
         <source>Join the development fund and support QuasarApp to help with the development of our projects.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="27"/>
+        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="50"/>
         <source>If you would like to use bitcoins to support us, you can transfer coins to these address. If you want to be included in the patron list, send an e-mail message - **QuasarApp@yandex.by**</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="33"/>
+        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="60"/>
         <source>**Note**: You must send a message signed with address of the bitcoin wallet from which the payment was made.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="40"/>
+        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="79"/>
         <source>**bitcoin:bc1q4wnp58plhx4fwnfnzz3e4slulv2gzzts89w2f7**</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="46"/>
+        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="89"/>
         <source># Our patrons list:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="52"/>
+        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="98"/>
         <source>## Silver Membership Patrons:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="58"/>
+        <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="106"/>
         <source>* Rustem Husnutdinov</source>
         <translation type="unfinished"></translation>
     </message>
