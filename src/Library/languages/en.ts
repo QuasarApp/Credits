b8d8fd12e925db4ded868e29a8c0c3efb2710dc1
Oleg-designer <sweetletal0@gmail.com>
2021-03-25 11:05:02 +0300

    fix #3 add translations for en ja ru tr ua languages

--- src/Library/languages/en.ts
+++ src/Library/languages/en.ts
@@ -1,42 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name>Credits</name>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="31"/>
         <source>Join the development fund and support QuasarApp to help with the development of our projects.</source>
-        <translation type="unfinished"></translation>
+        <translation>Join the development fund and support QuasarApp to help with the development of our projects.</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="50"/>
         <source>If you would like to use bitcoins to support us, you can transfer coins to these address. If you want to be included in the patron list, send an e-mail message - **QuasarApp@yandex.by**</source>
-        <translation type="unfinished"></translation>
+        <translation>If you would like to use bitcoins to support us, you can transfer coins to these address. If you want to be included in the patron list, send an e-mail message - **QuasarApp@yandex.by**</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="60"/>
         <source>**Note**: You must send a message signed with address of the bitcoin wallet from which the payment was made.</source>
-        <translation type="unfinished"></translation>
+        <translation>**Note**: You must send a message signed with address of the bitcoin wallet from which the payment was made.</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="79"/>
         <source>**bitcoin:bc1q4wnp58plhx4fwnfnzz3e4slulv2gzzts89w2f7**</source>
-        <translation type="unfinished"></translation>
+        <translation>**bitcoin:bc1q4wnp58plhx4fwnfnzz3e4slulv2gzzts89w2f7**</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="89"/>
         <source># Our patrons list:</source>
-        <translation type="unfinished"></translation>
+        <translation># Our patrons list:</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="98"/>
         <source>## Silver Membership Patrons:</source>
-        <translation type="unfinished"></translation>
+        <translation>## Silver Membership Patrons:</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="106"/>
         <source>* Rustem Husnutdinov</source>
-        <translation type="unfinished"></translation>
+        <translation>* Rustem Husnutdinov</translation>
     </message>
 </context>
 </TS>
--- src/Library/languages/ja.ts
+++ src/Library/languages/ja.ts
@@ -1,42 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="ja_JP">
 <context>
     <name>Credits</name>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="31"/>
         <source>Join the development fund and support QuasarApp to help with the development of our projects.</source>
-        <translation type="unfinished"></translation>
+        <translation>私たちのプロジェクトの開発を支援するために開発基金とサポートQuasarApp に参加。</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="50"/>
         <source>If you would like to use bitcoins to support us, you can transfer coins to these address. If you want to be included in the patron list, send an e-mail message - **QuasarApp@yandex.by**</source>
-        <translation type="unfinished"></translation>
+        <translation>あなたが私たちをサポートするためにビットコインを使用したい場合は、これらのアドレスにコインを転送す あなたがパトロンリストに含まれるようにしたい場合は、電子メールメッセージを送信-**QuasarApp@yandex.by**</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="60"/>
         <source>**Note**: You must send a message signed with address of the bitcoin wallet from which the payment was made.</source>
-        <translation type="unfinished"></translation>
+        <translation>**ノート**: 支払いが行われたbitcoinウォレットのアドレスで署名されたメッセージを送信する必要があります。</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="79"/>
         <source>**bitcoin:bc1q4wnp58plhx4fwnfnzz3e4slulv2gzzts89w2f7**</source>
-        <translation type="unfinished"></translation>
+        <translation>**ビットコイン:bc1q4wnp58plhx4fwnfnzz3e4slulv2gzzts89w2f7**</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="89"/>
         <source># Our patrons list:</source>
-        <translation type="unfinished"></translation>
+        <translation># 私たちの常連客リスト:</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="98"/>
         <source>## Silver Membership Patrons:</source>
-        <translation type="unfinished"></translation>
+        <translation>## シルバーメンバーシップ:</translation>
     </message>
     <message>
         <location filename="../src/QuasarAppCreditsModule/Credits.qml" line="106"/>
         <source>* Rustem Husnutdinov</source>
-        <translation type="unfinished"></translation>
+        <translation>* ルステム-フスヌットディノフ</translation>
     </message>
 </context>
 </TS>
