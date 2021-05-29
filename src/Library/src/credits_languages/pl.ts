be261bbbebb47711e7b65f7cfd395ac825eaab3a
Oleg-designer <sweetletal0@gmail.com>
2021-05-29 12:54:27 +0300

    ref #15 Add new languages and translation

---
+++ src/Library/src/credits_languages/pl.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl" sourcelanguage="en">
+<context>
+    <name>Credits</name>
+    <message>
+        <location filename="../QuasarAppCreditsModule/Credits.qml" line="31"/>
+        <source>Join the development fund and support QuasarApp to help with the development of our projects.</source>
+        <translation>Dołącz do funduszu rozwojowego i wesprzyj QuasarApp, aby pomóc w rozwoju naszych projektów.</translation>
+    </message>
+    <message>
+        <location filename="../QuasarAppCreditsModule/Credits.qml" line="50"/>
+        <source>If you would like to use bitcoins to support us, you can transfer coins to these address. If you want to be included in the patron list, send an e-mail message - **QuasarApp@yandex.by**</source>
+        <translation>Jeśli chcesz używać bitcoinów, aby nas wesprzeć, możesz przesyłać monety na ten adres. Jeśli chcesz znaleźć się na liście patronów, wyślij wiadomość e-mail - **QuasarApp@yandex.by**</translation>
+    </message>
+    <message>
+        <location filename="../QuasarAppCreditsModule/Credits.qml" line="60"/>
+        <source>**Note**: You must send a message signed with address of the bitcoin wallet from which the payment was made.</source>
+        <translation>**Uwaga**: Musisz wysłać wiadomość podpisaną adresem portfela bitcoin, z którego dokonano płatności.</translation>
+    </message>
+    <message>
+        <location filename="../QuasarAppCreditsModule/Credits.qml" line="79"/>
+        <source>**bitcoin:bc1q4wnp58plhx4fwnfnzz3e4slulv2gzzts89w2f7**</source>
+        <translation>**bitcoin:bc1q4wnp58plhx4fwnfnzz3e4slulv2gzzts89w2f7**</translation>
+    </message>
+    <message>
+        <location filename="../QuasarAppCreditsModule/Credits.qml" line="89"/>
+        <source># Our patrons list:</source>
+        <translation># Lista naszych patronów:</translation>
+    </message>
+    <message>
+        <location filename="../QuasarAppCreditsModule/Credits.qml" line="98"/>
+        <source>## Silver Membership Patrons:</source>
+        <translation>## Srebrni Patroni Członkostwa:</translation>
+    </message>
+    <message>
+        <location filename="../QuasarAppCreditsModule/Credits.qml" line="106"/>
+        <source>* Rustem Husnutdinov</source>
+        <translation>* Rustem Husnutdinov</translation>
+    </message>
+    <message>
+        <location filename="../QuasarAppCreditsModule/Credits.qml" line="114"/>
+        <source>* Semih Ufuk Güler</source>
+        <translation>* Semih Ufuk Güler</translation>
+    </message>
+</context>
+</TS>
